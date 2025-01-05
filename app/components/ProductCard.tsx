daa5dde10c7fae8cc64e2c5bdf1546b431e1052f
enlightwise <enlightwise@protonmail.com>
2025-01-05 13:31:49 +0100

    daisyUI

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
-    <div className="p-5 my-5 bg-slate-400 text-white text-xl hover:bg-slate-500">
+    <div>
       <AddToCart />
     </div>
   );
